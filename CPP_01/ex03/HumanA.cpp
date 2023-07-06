972747166c711d2a0df5c93f1d575e06933803d2
flo-12 <flobecht@hotmail.com>
2023-07-06 23:44:14 +0200

    changed pointer to weapon to ref to weapon for HumanA in ex03

--- CPP_01/ex03/HumanA.cpp
+++ CPP_01/ex03/HumanA.cpp
@@ -12,11 +12,8 @@
 
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string name, Weapon &weapon)
+HumanA::HumanA(std::string name, Weapon &weapon) : _name(name), _weapon(weapon)
 {
-	this->_name = name;
-	this->_weapon = &weapon;
-	return ;
 }
 
 HumanA::~HumanA(void)
@@ -27,6 +24,6 @@ HumanA::~HumanA(void)
 
 void	HumanA::attack(void)
 {
-	std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+	std::cout << this->_name << " attacks with their " << this->_weapon.getType() << std::endl;
 	return ;
 }
