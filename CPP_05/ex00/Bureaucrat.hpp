c92c9609200e7ede29eb8a9f8d18f8b91c3da959
flo-12 <flobecht@hotmail.com>
2023-09-05 19:54:02 +0200

    not too much...

--- CPP_05/ex00/Bureaucrat.hpp
+++ CPP_05/ex00/Bureaucrat.hpp
@@ -10,4 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
 
+# include <iostream>
+# include <exception>
+
+class Bureaucrat
+{
+private:
+	std::string const	_name;
+	int					_grade;
+public:
+	Bureaucrat();
+	Bureaucrat( const Bureaucrat &other );
+	~Bureaucrat();
+	Bureaucrat&	operator=( const Bureaucrat &other );
+
+	std::string	getName() const;
+	int			getGrade() const;
+
+	void		incGrade(); // e .g. from 3->2
+	void		decGrade();
+};
+
+std::ofstream	operator<<( const Bureaucrat lame );
+
+Bureaucrat::Bureaucrat( ) : _name("default"), _grade(50)
+{
+	std::cout << "Created Default Bureaucrat (name: " << _name << ", grade: " << _grade << ")" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
+
+
+#endif
