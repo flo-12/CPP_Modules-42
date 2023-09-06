54830b2b6ee68dabe9b4120757d788e6db657732
flo-12 <flobecht@hotmail.com>
2023-09-06 13:09:44 +0200

    ex01 finished

---
+++ CPP_05/ex01/Form.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbecht <fbecht@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 19:22:17 by fbecht            #+#    #+#             */
+/*   Updated: 2023/09/06 19:22:19 by fbecht           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <iostream>
+# include <exception>
+# include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class Form
+{
+private:
+	std::string const	_name;
+	bool				_signed;
+	int					_gradeSign;
+	int					_gradeExec;
+public:
+	Form();
+	Form( const Form &other );
+	Form( const std::string name, const bool s, const int gradeSign, const int gradeExec );
+	~Form();
+	Form&	operator=( const Form &other );
+
+	class GradeTooHighException : public std::exception {
+		virtual const char* what() const throw();
+	};
+	class GradeTooLowException : public std::exception {
+		virtual const char* what() const throw();
+	};
+
+	std::string	getName() const;
+	bool		getSigned() const;
+	int			getGradeSign() const;
+	int			getGradeExec() const;
+
+	void		beSigned( const Bureaucrat &bureau );
+};
+
+std::ostream&	operator<<( std::ostream &out, Form &form );
+
+
+#endif
---
+++ CPP_05/ex01/main.cpp
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbecht <fbecht@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 19:21:54 by fbecht            #+#    #+#             */
+/*   Updated: 2023/09/05 19:21:56 by fbecht           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int	main()
+{
+	std::cout << "------- CASES WITH WRONG GRADE -------" << std::endl;
+	try
+	{
+		Bureaucrat	p1( "Klaus", 0 );
+		std::cout << p1 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat	p2("Peter", 156);
+		std::cout << p2 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Form	f1("formly", false, -1, 20);
+		std::cout << f1 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		Form	f2("formly", false, 200, 20);
+		std::cout << f2 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		Form	f3("formly", false, 100, -20);
+		std::cout << f3 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		Form	f4("formly", false, 100, 198);
+		std::cout << f4 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+
+	std::cout << std::endl << std::endl << "------- CASES GRADE TOO LOW TO SIGN -------" << std::endl;
+	try
+	{
+		Bureaucrat	p3( "Tom", 100 );
+		Form		f5( "formly", false, 50, 10 );
+		std::cout << p3 << std::endl;
+		std::cout << f5 << std::endl;
+		p3.signForm( f5 );
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+
+	std::cout << std::endl << std::endl << "------- CASE SUCCESS SIGN -------" << std::endl;
+	try
+	{
+		Bureaucrat	p4( "Eddy", 10 );
+		Form		f6( "formly", false, 50, 10 );
+		std::cout << p4 << std::endl;
+		std::cout << f6 << std::endl;
+		p4.signForm( f6 );
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	return (0);
+}
