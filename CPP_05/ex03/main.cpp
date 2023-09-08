001653f90753ed0736f3bbe1b608be07ce0f4e45
flo-12 <flobecht@hotmail.com>
2023-09-08 10:44:32 +0200

    update names in Makefile

--- CPP_05/ex03/main.cpp
+++ CPP_05/ex03/main.cpp
@@ -14,69 +14,45 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "Intern.hpp"
 
 int	main()
 {
-	{
+	Intern	intern;
+	AForm	*pf;
+	AForm	*rf;
+	AForm	*sf;
+
+	pf = intern.makeForm( "Something New", "home" );
+	if ( !pf )
+		pf = intern.makeForm( "PresidentialPardonForm", "home" );
+	rf = intern.makeForm( "RobotomyRequestForm", "homeAgain" );
+	sf = intern.makeForm( "ShrubberyCreationForm", "Desktop" );
+
 	std::cout << "------- CASES: SUCCESS PresidentialPardon -------" << std::endl;
 	Bureaucrat	p1( "Klaus", 5 );
-	PresidentialPardonForm	pf( "home" );
+	//PresidentialPardonForm	pf( "home" );
 	std::cout << p1 << std::endl;
-	std::cout << pf << std::endl;
-	p1.signForm( pf );
-	p1.executeForm( pf );
+	std::cout << *pf << std::endl;
+	p1.signForm( *pf );
+	p1.executeForm( *pf );
 
 	std::cout << "------- CASES: SUCCESS RobotomyRequest -------" << std::endl;
 	Bureaucrat	p2("Peter", 15);
-	RobotomyRequestForm	rf( "homeAgain" );
+	//RobotomyRequestForm	rf( "homeAgain" );
 	std::cout << p2 << std::endl;
-	std::cout << rf << std::endl;
-	p2.signForm( rf );
-	p2.executeForm( rf );
+	std::cout << *rf << std::endl;
+	p2.signForm( *rf );
+	p2.executeForm( *rf );
 	
 	std::cout << "------- CASES: SUCCESS ShrubberyCreation -------" << std::endl;
 	Bureaucrat	p3( "Wolfgang", 118 );
-	ShrubberyCreationForm	sf( "Desktop" );
+	//ShrubberyCreationForm	sf( "Desktop" );
 	std::cout << p3 << std::endl;
-	std::cout << sf << std::endl;
-	p3.signForm( sf );
-	p3.executeForm( sf );
-	}
+	std::cout << *sf << std::endl;
+	p3.signForm( *sf );
+	p3.executeForm( *sf );
 
-	{
-	std::cout << std::endl << "------- CASES: NO SUCCESS Exec PresidentialPardon -------" << std::endl;
-	Bureaucrat	p1( "Klaus", 10 );
-	Bureaucrat	p11( "Klaus the 2nd", 3 );
-	PresidentialPardonForm	pf( "home2" );
-	std::cout << p1 << std::endl;
-	std::cout << p11 << std::endl;
-	std::cout << pf << std::endl;
-	p11.executeForm( pf );
-	p1.signForm( pf );
-	p1.executeForm( pf );
-
-	std::cout << "------- CASES: NO SUCCESS Exec RobotomyRequest -------" << std::endl;
-	Bureaucrat	p2("Peter", 55);
-	Bureaucrat	p22("Peter the 2nd", 35);
-	RobotomyRequestForm	rf( "homeAgain2" );
-	std::cout << p2 << std::endl;
-	std::cout << p22 << std::endl;
-	std::cout << rf << std::endl;
-	p22.executeForm( rf );
-	p2.signForm( rf );
-	p2.executeForm( rf );
-	
-	std::cout << "------- CASES: NO SUCCESS Exec ShrubberyCreation -------" << std::endl;
-	Bureaucrat	p3( "Wolfgang", 140 );
-	Bureaucrat	p33( "Wolfgang the 2nd", 14 );
-	ShrubberyCreationForm	sf( "Desktop2" );
-	std::cout << p3 << std::endl;
-	std::cout << p33 << std::endl;
-	std::cout << sf << std::endl;
-	p33.executeForm( sf );
-	p3.signForm( sf );
-	p3.executeForm( sf );
-	}
 		
 	return (0);
 }
